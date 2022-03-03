064bb4ed6e74a2ab17825ac8dfa05529e2f1649f
Raulillo <36534958+r4ulill0@users.noreply.github.com>
2022-03-03 20:27:04 +0100

    Añadida signature de autocompletado y refactorizada la configuración del autocompletado para añadirse dentro de cada servidor de lenguajes

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -47,12 +47,13 @@ Plug 'mfussenegger/nvim-jdtls'          -- Language server para java
 
 Plug 'neovim/nvim-lspconfig'            -- Configs for lsp
 
-Plug 'hrsh7th/cmp-nvim-lsp'             -- Autocompletado
-Plug 'hrsh7th/cmp-buffer'               --|
-Plug 'hrsh7th/cmp-path'                 --|
-Plug 'hrsh7th/cmp-cmdline'              --|
-Plug 'hrsh7th/nvim-cmp'                 --|
-Plug 'SirVer/ultisnips'                 -- Plugin de snipets super completo
+Plug 'hrsh7th/cmp-nvim-lsp'                -- Autocompletado
+Plug 'hrsh7th/cmp-buffer'                  --|
+Plug 'hrsh7th/cmp-path'                    --|
+Plug 'hrsh7th/cmp-cmdline'                 --|
+Plug 'hrsh7th/cmp-nvim-lsp-signature-help' --|
+Plug 'hrsh7th/nvim-cmp'                    --|
+Plug 'SirVer/ultisnips'                    -- Plugin de snipets super completo
 Plug 'quangnguyen30192/cmp-nvim-ultisnips' -- Adaptador del plugin para nvim
 
 vim.call('plug#end')
@@ -76,10 +77,10 @@ vim.opt.completeopt = {'menu', 'menuone', 'noselect'}               -- Obliga al
 -- Mappings.
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
 local opts = { noremap=true, silent=true }
-vim.api.nvim_set_keymap('n', '<space>e', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
+vim.api.nvim_set_keymap('n', '<leader>e', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
 vim.api.nvim_set_keymap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
 vim.api.nvim_set_keymap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
-vim.api.nvim_set_keymap('n', '<space>q', '<cmd>lua vim.diagnostic.setloclist()<CR>', opts)
+vim.api.nvim_set_keymap('n', '<leader>q', '<cmd>lua vim.diagnostic.setloclist()<CR>', opts)
 
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
@@ -94,31 +95,15 @@ local on_attach = function(client, bufnr)
   vim.api.nvim_buf_set_keymap(bufnr, 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
   vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
   vim.api.nvim_buf_set_keymap(bufnr, 'n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
   vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>f', '<cmd>lua vim.lsp.buf.formatting()<CR><cmd>echo \'hola\'<CR>', opts)
 end
-
--- Use a loop to conveniently call 'setup' on multiple servers and
--- map buffer local keybindings when the language server attaches
-local servers = { 'jedi_language_server' }
-for _, lsp in pairs(servers) do
-  require('lspconfig')[lsp].setup {
-    on_attach = on_attach,
-    flags = {
-      -- This will be the default in neovim 0.7+
-      debounce_text_changes = 150,
-    }
-  }
-end
-
--- CONFIGURACION LSP PYTHON "JEDI"
-require('lspconfig').jedi_language_server.setup({})
 
 -- CONFIGURACION DE AUTOCOMPLETADO (CMP)
 local cmp = require 'cmp'
@@ -141,8 +126,9 @@ cmp.setup({
         ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
     },
     sources = cmp.config.sources({
-        { name = 'nvim_lsp' },
-        { name = 'ultisnips' }, -- For ultisnips users.
+          { name = 'nvim_lsp' },
+          { name = 'ultisnips' }, -- For ultisnips users.
+          { name = 'nvim_lsp_signature_help' },
         }, {
         { name = 'buffer' },
     })
@@ -175,10 +161,21 @@ sources = cmp.config.sources({
 
 -- Setup lspconfig.
 local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
--- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
-require('lspconfig')['jedi_language_server'].setup {
-capabilities = capabilities
-}
+
+-- Use a loop to conveniently call 'setup' on multiple servers and
+-- map buffer local keybindings when the language server attaches
+local servers = { 'jedi_language_server' }
+for _, lsp in pairs(servers) do
+  require('lspconfig')[lsp].setup {
+    on_attach = on_attach,
+    capabilities = capabilities,
+    flags = {
+      -- This will be the default in neovim 0.7+
+      debounce_text_changes = 150,
+    }
+  }
+end
+
 
 -- CONFIGURACION DE COLORINES
 vim.opt.termguicolors=true
