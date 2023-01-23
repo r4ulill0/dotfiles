97364ede340e7e145d0ce10f760e44afcc6e0adc
Raulillo <36534958+r4ulill0@users.noreply.github.com>
2023-01-23 19:50:49 +0100

    WIP: linkeo automatico de archivos del sistema para nuevas instalaciones

---
+++ scripts/systemLinker.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# TODO Comprobar que el directorio actual tiene acceso directo a .dotfiles y si no cerrar
+
+# TODO poner un modo de ejecución para eliminar el archivo existente y sustituirlo por el enlace
+# TODO asegurarse de que se usan rutas absolutas o fallará por no llegar al archivo de dotfiles
+ln -s $HOME/.dotfiles/.bashrc .bashrc
+ln -s $HOME/.dotfiles/.config/ .config
+ln -s $HOME/.dotfiles/.xinitrc .xinitrc
