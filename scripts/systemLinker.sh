90e4cb1f3bf588dab16df6460ab6a01af3f19e74
Raulillo <36534958+r4ulill0@users.noreply.github.com>
2023-01-23 20:41:42 +0100

    Actualizado linkeo para iniciar el entorno en modo GUI

--- scripts/systemLinker.sh
+++ scripts/systemLinker.sh
@@ -5,5 +5,6 @@
 # TODO poner un modo de ejecución para eliminar el archivo existente y sustituirlo por el enlace
 # TODO asegurarse de que se usan rutas absolutas o fallará por no llegar al archivo de dotfiles
 ln -s $HOME/.dotfiles/.bashrc .bashrc
+ln -s $HOME/.dotfiles/.bash_profile .bash_profile
 ln -s $HOME/.dotfiles/.config/ .config
 ln -s $HOME/.dotfiles/.xinitrc .xinitrc
