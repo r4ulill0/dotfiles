d6d00ae8d6d234ed95dafca53291b9f02039009d
Raulillo <36534958+r4ulill0@users.noreply.github.com>
2023-02-13 21:46:19 +0100

    Actualiza api deprecada

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -86,7 +86,7 @@ sources = cmp.config.sources({
 })
 
 -- Setup lspconfig.
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
 -- Use a loop to conveniently call 'setup' on multiple servers and
 -- map buffer local keybindings when the language server attaches
