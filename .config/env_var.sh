59971d4bf52412a3367337a0459b0b46a2b352e5
Raulillo <36534958+r4ulill0@users.noreply.github.com>
2023-06-19 20:20:28 +0200

    Añadido IMF (fcitx) y IME (mocz) para poder manejar el japonés

--- .config/env_var.sh
+++ .config/env_var.sh
@@ -7,3 +7,6 @@ export XCOMPOSEFILE=$XDG_CONFIG_HOME/.XCompose
 export GTK_IM_MODULE=xim
 export BACKUPDIRS=$HOME/gestion/docs
 export CALENDARS_DIR=$HOME/gestion/docs/calendarios
+export GTK_IM_MODULE=fcitx
+export QT_IM_MODULE=fcitx
+export XMODIFIERS=@im=fcitx
